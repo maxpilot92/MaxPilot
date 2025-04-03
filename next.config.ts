1f02f810b44b687b3589d780b8767cd5ce29d273
StudyDichan <dichan.shresthas24@cps.edu.np>
2025-04-03 13:40:21 +0545

    add

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [{ key: "company-id", value: ":company-id" }],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
